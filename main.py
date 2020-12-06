191ab071daeff7440249d664f3421a296ed8588a
Divyanshu <kumardivy1999@gmail.com>
2020-12-06 10:56:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from flask import Flask, render_template, request
 from sklearn.naive_bayes import GaussianNB
 from sklearn.feature_extraction.text import CountVectorizer
